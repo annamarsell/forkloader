f0eb6a039c838721a9c27a7a1e20a48c05fd5ff8
Anna Mårsell <anna.marsell@triona.se>
2015-12-06 21:54:59 +0100

    Initial version
    Initial version

---
+++ ForkLoader/ForkKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkLoader
+{
+    public class ForkKey
+    {
+        public int TeamNumber { get; set; }
+        public int ClassId { get; set; }
+        public List<string> Forks { get; set; }
+    }
+}
---
+++ ForkLoader/ForkKeyReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ForkLoader
+{
+    public class ForkKeyReader
+    {
+        private readonly string m_filename;
+        private static readonly ILog m_loggger = LogManager.GetLogger(typeof (ForkKeyReader));
+
+        public ForkKeyReader(string filename)
+        {
+            m_filename = filename;
+        }
+
+        public List<ForkKey> ReadForkKeys()
+        {
+            var forkKeys = new List<ForkKey>();
+            using(var sr = new StreamReader(m_filename))
+            {
+                string forkKeyString;
+                while (!string.IsNullOrEmpty(forkKeyString = sr.ReadLine()))
+                {
+                    ForkKey forkKey = new ForkKey();
+                    string[] parts = forkKeyString.Split(new char[] {';'});
+                    try
+                    {
+                        forkKey.TeamNumber = Convert.ToInt32(parts[0]);
+                    }
+                    catch (Exception)
+                    {
+                        if(m_loggger.IsErrorEnabled) m_loggger.ErrorFormat("{0} does not begin with a valid team number.", forkKeyString);
+                        throw;
+                    }
+                    forkKey.Forks = new List<string>();
+                    foreach (string part in parts)
+                    {
+                        forkKey.Forks.Add(part);
+                    }
+                }
+            }
+            return forkKeys;
+        }
+    }
+}
