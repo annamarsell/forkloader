b021d014690a768e8e29ae4e935f68c564e8f839
Anna Mårsell <anna.marsell@triona.se>
2016-01-18 21:36:31 +0100

    Started writing writer

--- ForkLoader/OLAWriter.cs
+++ ForkLoader/OLAWriter.cs
@@ -19,22 +19,66 @@ namespace ForkLoader
         // Update individualCourseId, forkedCourseId for the entry in the results table that has the raceClassId given by the 
         // raceClasses table keyed by startNumberBase and relayLeg
 
-        public OlaWriter(string connectionString)
+        private readonly string m_connectionString;
+        private readonly int m_eventClassId;
+        public OlaWriter(string connectionString, int eventClassId)
         {
-            MySql.Data.MySqlClient.MySqlConnection conn;
+            m_connectionString = connectionString;
+            m_eventClassId = eventClassId;
+        }
+
+        public void WriteForkKeys(List<ForkKey> forkKeys)
+        {
+            List<RaceClass> raceClasses = ReadRaceClasses();
+            MySql.Data.MySqlClient.MySqlConnection conn = null;
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(m_connectionString);
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                foreach (ForkKey forkKey in forkKeys)
+                {
+                    for (int leg = 1; leg <= forkKey.Forks.Count; leg++)
+                    {
+                        int raceClassId =
+                            raceClasses.Single(r => r.StartNumberBase == forkKey.TeamNumber && r.RelayLeg == leg)
+                                .RaceClassId;
+                        cmd.CommandText = "update results set individualCourseId = " + CourseNameToId(forkKey.Forks[leg-1]) +
+                            ", forkedCourseId = " + CourseNameToId(forkKey.Forks[leg - 1]) + " where raceClassId = " + raceClassId;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            } 
             string myConnectionString;
 
             myConnectionString = "server=127.0.0.1;uid=root;" +
                 "pwd=12345;database=dmstafett;";
 
+            
+        }
+
+        private List<RaceClass> ReadRaceClasses()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn = null;
             try
             {
-                conn = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+                conn = new MySql.Data.MySqlClient.MySqlConnection(m_connectionString);
                 conn.Open();
                 IDbCommand cmd = conn.CreateCommand();
-                cmd.CommandText = "select * from raceclasses";
+                cmd.CommandText = "select * from raceclasses where eventClassId = " + m_eventClassId;
                 IDataReader reader = cmd.ExecuteReader();
-                List<RaceClass> raceClasses = new List<RaceClass>();
+                var raceClasses = new List<RaceClass>();
                 while (reader.Read())
                 {
                     if (reader["eventClassId"] != null && reader["eventClassId"] != DBNull.Value &&
@@ -47,10 +91,42 @@ namespace ForkLoader
                             Convert.ToInt32(reader["startNumberBase"]), Convert.ToInt32(reader["relayLeg"])));
                     }
                 }
+                return raceClasses;
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return null;
+        }
+
+        private int CourseNameToId(string name)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn = null;
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(m_connectionString);
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from raceclasscourses where eventClassId = " + m_eventClassId;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
             }
         }
     }
